e75486aebc8046c9c4016b55fdfb3d274ed4e7db
TheBattlePug <dan.dumitrescu@live.com>
2022-06-27 23:02:35 -0700

    added the website stuff

---
+++ run.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route("/page_1")
+def page1():
+	return render_template('page1.html')
+
+@app.route("/page_2")
+def page2():
+	return render_template('page2.html')
+
+@app.route("/page_3")
+def page3():
+	return render_template('page3.html')
+
