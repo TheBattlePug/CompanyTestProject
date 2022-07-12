efea7ea2e3cfa2b67d1c2962b9046a2885f050ee
TheBattlePug <dan.dumitrescu@live.com>
2022-07-11 22:04:12 -0700

    fixed a typo in run.py

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request, redirect
+import pymongo
 
 app = Flask(__name__)
+myclient = pymongo.MongoClient("mongodb://mongo:27017")
+
+@app.route("/")
+def default():
+	return render_template("page1.html")
 
 @app.route("/page_1")
 def page1():
@@ -15,5 +21,5 @@ def page3():
 	return render_template('page3.html')
 
 
-if __name__ == '__main--':
+if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=4000, debug=True)
